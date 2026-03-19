c4ebf09b23e6f779162c192b08ccb69f103b94cc
Olga Bilokur <olyanderbilokur@gmail.com>
2026-03-19 12:22:13 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -6,20 +6,20 @@
 function calculateRentalCost(days) {
   const costPerDay = 40;
   const costForAllDay = costPerDay * days;
+  const countDaysForSaleFirst = 3;
+  const countDaysForSaleSecond = 7;
   const saleForThreeDays = 20;
   const saleForSevenDays = 50;
 
-  if (days >= 7) {
+  if (days >= countDaysForSaleSecond) {
     return costForAllDay - saleForSevenDays;
   }
 
-  if (days >= 3) {
+  if (days >= countDaysForSaleFirst) {
     return costForAllDay - saleForThreeDays;
   }
 
-  if (days < 3) {
-    return costForAllDay;
-  }
+  return costForAllDay;
 }
 
 module.exports = calculateRentalCost;
